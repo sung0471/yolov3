16e67a23f0092d0e8860a4fec6b155408acaedeb
sung0471 <zzererer@naver.com>
2020-12-16 19:00:52 +0900

    201216 commit - 1 utils/parse_config.py
    - cfg 파일을 읽어서 저장한 list를 -> json으로 dump하는 코드추가
    - cfg를 json으로 dump한 dict을 -> load하는 코드추가

--- utils/parse_config.py
+++ utils/parse_config.py
@@ -1,20 +1,31 @@
-def parse_model_config(path):
-    file = open(path, 'r')
-    lines = file.read().split('\n')
-    lines = [x for x in lines if x and not x.startswith('#')]
-    lines = [x.strip() for x in lines]    # get rid of fringe whitespaces
-    module_defs = []
+import json
+import os
 
-    for line in lines:
-        if line.startswith('['):    # new block을 mask함
-            module_defs.append({})
-            module_defs[-1]['type'] = line[1:-1].rstrip()
-            if module_defs[-1]['type'] == 'convolutional':
-                module_defs[-1]['batch_normalize'] = 0
-        else:
-            key, value = line.split('=')
-            value = value.strip()
-            module_defs[-1][key.rstrip()] = value.strip()
+
+def parse_model_config(path):
+    module_defs = []
+    new_path = '.'.join(path.split('.')[:-1]) + '.json'
+    if not os.path.exists(new_path):
+        file = open(path, 'r')
+        lines = file.read().split('\n')
+        lines = [x for x in lines if x and not x.startswith('#')]
+        lines = [x.strip() for x in lines]    # get rid of fringe whitespaces
+
+        for line in lines:
+            if line.startswith('['):    # new block을 mask함
+                module_defs.append({})
+                module_defs[-1]['type'] = line[1:-1].rstrip()
+                if module_defs[-1]['type'] == 'convolutional':
+                    module_defs[-1]['batch_normalize'] = 0
+            else:
+                key, value = line.split('=')
+                value = value.strip()
+                module_defs[-1][key.rstrip()] = value.strip()
+
+        json.dump({'data': module_defs}, open(new_path, 'w'), indent=2)
+    else:
+        decode = json.load(open(new_path, 'r'))
+        module_defs = decode['data']
 
     return module_defs
 
